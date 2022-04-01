846a90990335cd0dda46bc7ed0ac5aa028b50ab7
bona <bona2324@gmail.com>
2022-04-01 22:58:54 +0900

    js : calculate, clear 추가

--- claculator.js
+++ claculator.js
@@ -6,14 +6,16 @@ const $result = document.querySelector("#result");
 
 //* 숫자 저장하기 (버튼 안의 문자를 가져와서 코드 줄이기)
 const onClickNumber = (event) => {
-    if (operator) {
-        if (!numTwo) {
-            $result.value = ""; //^ numTwo에 값이 없을 때 numTwo를 입력하면 화면 비우기
-        }
-        numTwo += event.target.textContent;
-    } else {
+    if (!operator) {
         numOne += event.target.textContent;
+        $result.value += event.target.textContent;
+        return;
     }
+    //^ 이 아래로는 operator가 존재하는 경우에만 실행됨 => else 감쌀필요 없으므로 삭제
+    if (!numTwo) {
+        $result.value = ""; //^ numTwo에 값이 없을 때 numTwo를 입력하면 화면 비우기
+    }
+    numTwo += event.target.textContent;
     $result.value += event.target.textContent;
 };
 document.querySelector("#num-0").addEventListener("click", onClickNumber);
@@ -40,5 +42,34 @@ document.querySelector("#plus").addEventListener("click", onClickOperator("+"));
 document.querySelector("#minus").addEventListener("click", onClickOperator("-"));
 document.querySelector("#divide").addEventListener("click", onClickOperator("/"));
 document.querySelector("#multiply").addEventListener("click", onClickOperator("*"));
-document.querySelector("#calculate").addEventListener("click", () => {});
-document.querySelector("#plus").addEventListener("click", () => {});
+
+document.querySelector("#calculate").addEventListener("click", () => {
+    if (numTwo) {
+        switch (operator) {
+            case "+":
+                $result.value = parseInt(numOne) + parseInt(numTwo); //^ 빼기, 곱하기, 나누기 연산자를 사용할 때에는 자동으로 문자열을 숫자로 바꾼 후 계산
+                break;
+            case "-":
+                $result.value = numOne - numTwo;
+                break;
+            case "*":
+                $result.value = numOne * numTwo;
+                break;
+            case "/":
+                $result.value = numOne / numTwo;
+                break;
+            default:
+                break;
+        }
+    } else {
+        alert("숫자를 먼저 입력하세요.");
+    }
+});
+
+document.querySelector("#clear").addEventListener("click", () => {
+    numOne = "";
+    operator = "";
+    numTwo = "";
+    $operator.value = "";
+    $result.value = "";
+});
